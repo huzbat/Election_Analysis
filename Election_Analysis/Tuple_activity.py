e628c28ea4fb815da0ec9edf646e5555d66e6eef
huzbat <87838015+huzbat@users.noreply.github.com>
2022-07-17 18:29:19 -0400

    Add files via upload

---
+++ Election_Analysis/Tuple_activity.py
@@ -0,0 +1,10 @@
+# my_tuple = ( )
+# counties_tuples = ("Arapahoe","Denver","Jefferson")
+# len(counties_tuples)
+# print(counties_tuples)
+
+counties = ['Arapahoe', 'Denver', 'El Paso', 'Jefferson', 'El Paso']
+#Remove 2nd El Pase
+# counties.remove('El Paso')
+counties.pop(4)
+print(counties)
